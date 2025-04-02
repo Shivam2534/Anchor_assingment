3ee4be590d8ee2cbdd16275f8decafa506e29b10
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2025-04-02 17:10:32 +0530

    AWS backend url updated, pipeline works fine

--- frontend/src/contant.ts
+++ frontend/src/contant.ts
@@ -1,1 +1,4 @@
 export const Backend_URL = "https://anchor-assingment.onrender.com";
+
+// This is a Deployed URL of backend on AWS
+// export const Backend_URL = "http://ec2-3-110-121-231.ap-south-1.compute.amazonaws.com:5000/";
